f89cd36894c0ca1c36b2fc77cda450f9b734a4c7
cieloazul310 <cieloazul310@gmail.com>
2022-01-11 20:43:39 +0900

    #7: update

--- packages/vt-style-aoi/src/layers/label/index.ts
+++ packages/vt-style-aoi/src/layers/label/index.ts
@@ -2,6 +2,7 @@ import Style from 'ol/style/Style';
 import Text from 'ol/style/Text';
 import Fill from 'ol/style/Fill';
 import Stroke from 'ol/style/Stroke';
+import Circle from 'ol/style/Circle';
 import { FeatureLike } from 'ol/Feature';
 import { isNumber, isString, dspPosToPosition } from '../../utils';
 
@@ -10,16 +11,38 @@ export default function labelStyle(feature: FeatureLike) {
   if (!isNumber(ftCode)) throw new Error();
   if (!isNumber(annoCtg)) throw new Error();
   if (!isString(knj)) throw new Error();
-  const { textAlign, textBaseline, offsetX, offsetY } =
-    dspPosToPosition(dspPos);
+
+  if (annoCtg > 880) {
+    return new Style({
+      image: new Circle({ radius: 4, fill: new Fill({ color: '#f00' }) }),
+    });
+  }
+
+  const position = dspPosToPosition(dspPos);
+
+  const color = [110, 120, 130, 140, 210, 220].includes(annoCtg)
+    ? '#333'
+    : [321, 322, 323, 344, 345, 347, 348].includes(annoCtg)
+    ? '#77d'
+    : [311, 312, 314, 315, 316, 331, 332, 333].includes(annoCtg)
+    ? '#744'
+    : '#777';
+  const fontSize = [110, 140, 333].includes(annoCtg)
+    ? 18
+    : [344, 345].includes(annoCtg)
+    ? 14
+    : 12;
+  const stroke = [110, 120, 140, 333].includes(annoCtg)
+    ? new Stroke({ color: '#fff', width: 4 })
+    : undefined;
+
   return new Style({
     text: new Text({
       text: knj,
-      fill: new Fill({ color: '#777' }),
-      textAlign,
-      textBaseline,
-      offsetX,
-      offsetY,
+      fill: new Fill({ color }),
+      font: `${fontSize}px sans-serif`,
+      stroke,
+      ...position,
     }),
     zIndex: 900,
   });
--- packages/vt-style-aoi/src/layers/symbol/index.ts
+++ packages/vt-style-aoi/src/layers/symbol/index.ts
@@ -8,7 +8,7 @@ import { FeatureLike } from 'ol/Feature';
 import { isNumber, dspPosToPosition } from '../../utils';
 
 export default function symbolStyle(feature: FeatureLike, resolution: number) {
-  const { ftCode, knj, dspPos } = feature.getProperties();
+  const { ftCode, knj, name, dspPos } = feature.getProperties();
   if (!isNumber(ftCode)) throw new Error();
 
   if ([51301, 51302, 51303].includes(ftCode)) {
@@ -28,6 +28,39 @@ export default function symbolStyle(feature: FeatureLike, resolution: number) {
           fill: new Fill({ color: '#333' }),
           stroke: new Stroke({ width: 2, color: '#fff' }),
           font: `bold ${radius + 10}px sans-serif`,
+          textAlign,
+          textBaseline,
+          offsetX,
+          offsetY,
+        }),
+        zIndex: zIndex + 10,
+      }),
+      new Style({
+        image: new Circle({
+          radius,
+          fill: new Fill({ color: '#fff' }),
+          stroke: new Stroke({ width: 1, color: '#333' }),
+        }),
+        zIndex,
+      }),
+    ];
+  }
+
+  if ([1401, 1402, 1403].includes(ftCode)) {
+    const zIndex = ftCode === 1401 ? 1002 : ftCode === 1402 ? 1001 : 1000;
+    const radius = ftCode === 1401 ? 4 : ftCode === 1402 ? 3 : 2;
+    const { textAlign, textBaseline, offsetX, offsetY } = dspPosToPosition(
+      dspPos,
+      radius
+    );
+
+    return [
+      new Style({
+        text: new Text({
+          text: name as string,
+          fill: new Fill({ color: '#666' }),
+          stroke: new Stroke({ width: 2, color: '#fff' }),
+          font: `${radius + 10}px sans-serif`,
           textAlign,
           textBaseline,
           offsetX,
