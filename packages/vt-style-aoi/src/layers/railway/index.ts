5e4fea25926fc9b05dd362326ecf9d77dde680f1
cieloazul310 <cieloazul310@gmail.com>
2022-01-12 16:02:25 +0900

    #13: update

--- packages/vt-style-aoi/src/layers/railway/index.ts
+++ packages/vt-style-aoi/src/layers/railway/index.ts
@@ -1,13 +1,13 @@
 import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
 import { FeatureLike } from 'ol/Feature';
-
 import { isNumber } from '../../utils';
 
 export default function railwayStyle(feature: FeatureLike, resolution: number) {
-  const { ftCode, snglDbl, rtCode10, lvOrder } = feature.getProperties();
+  const { ftCode, snglDbl, rtCode, rtCode1, rtCode10, lvOrder, staCode } =
+    feature.getProperties();
   if (!isNumber(ftCode)) throw new Error();
-
+  // console.log(rtCode, rtCode1, rtCode10);
   if ([58201, 58203, 58204].includes(ftCode)) {
     const width = ftCode === 58201 ? 1 : 2;
     const color =
@@ -37,28 +37,46 @@ export default function railwayStyle(feature: FeatureLike, resolution: number) {
     return new Style({
       stroke: new Stroke({
         width: 4,
+        color: '#f69',
+      }),
+      zIndex: 200,
+    });
+  } else if (staCode && staCode !== '0') {
+    return new Style({
+      stroke: new Stroke({
+        width: 4,
         color: '#666',
       }),
-      zIndex: 10,
+      zIndex: 200,
     });
   } else {
     const width =
-      resolution > 50
+      resolution > 152.87
         ? 1
-        : rtCode10 === '2'
+        : rtCode10 === '1'
         ? 4
         : snglDbl === 1
-        ? 2
+        ? 1
         : snglDbl === 2
-        ? 3
+        ? 2
         : 1;
-    const color = rtCode10 === '2' ? '#666' : '#666';
-    return new Style({
-      stroke: new Stroke({
-        width,
-        color,
+    const color = rtCode10 === '1' ? '#66d' : '#666';
+
+    return [
+      new Style({
+        stroke: new Stroke({
+          width,
+          color,
+        }),
+        zIndex: 160 + (lvOrder ?? 0) * 10,
       }),
-      zIndex: 150 + (lvOrder ?? 0) * 10,
-    });
+      new Style({
+        stroke: new Stroke({
+          width: width + 2,
+          color: '#fff',
+        }),
+        zIndex: 150 + (lvOrder ?? 0) * 10,
+      }),
+    ];
   }
 }
