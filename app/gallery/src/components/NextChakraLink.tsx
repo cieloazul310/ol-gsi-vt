729a22b0855f08e69a4eb9e01fd8e5a4b4c14900
cieloazul310 <cieloazul310@gmail.com>
2023-09-01 16:25:47 +0900

    feat(gallery): update to next v13

--- app/gallery/src/components/NextChakraLink.tsx
+++ app/gallery/src/components/NextChakraLink.tsx
@@ -1,17 +1,7 @@
-// https://github.com/chakra-ui/chakra-ui/blob/main/examples/nextjs-typescript/components/NextChakraLink.tsx
-import { PropsWithChildren } from 'react';
-import NextLink from 'next/link';
-import { LinkProps as NextLinkProps } from 'next/dist/client/link';
-import {
-  Link as ChakraLink,
-  LinkProps as ChakraLinkProps,
-} from '@chakra-ui/react';
+import { Link, type LinkProps } from "@chakra-ui/next-js";
 
-export type NextChakraLinkProps = PropsWithChildren<
-  NextLinkProps & Omit<ChakraLinkProps, 'as'>
->;
+export type NextChakraLinkProps = LinkProps;
 
-//  Has to be a new component because both chakra and next share the `as` keyword
 function NextChakraLink({
   href,
   as,
@@ -23,33 +13,32 @@ function NextChakraLink({
   ...chakraProps
 }: NextChakraLinkProps) {
   // https://github.com/hupe1980/gatsby-theme-material-ui/blob/master/packages/gatsby-material-ui-components/src/glink.tsx
-  const internal = /^\/(?!\/)/.test(href);
+  const internal = /^\/(?!\/)/.test(href.toString());
 
   if (internal) {
     return (
-      <NextLink
-        passHref={true}
+      <Link
         href={href}
-        as={as}
         replace={replace}
         scroll={scroll}
         shallow={shallow}
         prefetch={prefetch}
+        {...chakraProps}
       >
-        <ChakraLink {...chakraProps}>{children}</ChakraLink>
-      </NextLink>
+        {children}
+      </Link>
     );
   }
 
   return (
-    <ChakraLink
+    <Link
       href={href}
       target="_blank"
       rel="noopener noreferrer"
       {...chakraProps}
     >
       {children}
-    </ChakraLink>
+    </Link>
   );
 }
 
