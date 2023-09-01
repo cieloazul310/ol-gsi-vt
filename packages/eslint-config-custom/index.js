96a514f528cc23d945d084b34c6179c4526db675
cieloazul310 <cieloazul310@gmail.com>
2023-09-01 22:32:39 +0900

    chore: add eslint-config-custom and tsconfig package

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "prettier",
+  ],
+  plugins: ["jest"],
+  parser: "@typescript-eslint/parser",
+};
