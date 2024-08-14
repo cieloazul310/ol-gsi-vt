e0aa8a92f07ff8bf121d61c4d8f882cdd1076aa7
cieloazul310 <cieloazul310@gmail.com>
2024-08-14 16:57:44 +0900

    chore(eslint): remove jest plugins

--- packages/eslint-config-custom/index.cjs
+++ packages/eslint-config-custom/index.cjs
@@ -1,16 +1,15 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
   env: {
     es6: true,
     node: true,
-    "jest/globals": true,
   },
   extends: [
     "airbnb-base",
     "airbnb-typescript/base",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
     "prettier",
   ],
-  plugins: ["jest"],
   parser: "@typescript-eslint/parser",
 };
