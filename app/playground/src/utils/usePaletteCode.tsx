570cec79348b96857fe4374c6cf814206ff47c42
cieloazul310 <cieloazul310@gmail.com>
2024-08-19 22:14:59 +0900

    fix(style)!: fix layer - NEVER USE v1.4.4

---
+++ app/playground/src/utils/usePaletteCode.tsx
@@ -0,0 +1,23 @@
+import { useMemo } from "react";
+import { diff } from "deep-object-diff";
+import { useDefaultPalette, usePalePalette } from "@cieloazul310/ol-gsi-vt";
+import { usePaletteStore } from "@/providers/palette-provider";
+
+function isEmpty(obj: Record<string, any>) {
+  return Object.keys(obj).length === 0;
+}
+
+function usePaletteCode() {
+  const { palette, paletteType } = usePaletteStore((store) => store);
+
+  return useMemo(() => {
+    const basePalette =
+      paletteType === "pale" ? usePalePalette() : useDefaultPalette();
+    const value = diff(basePalette, palette);
+    const status = !isEmpty(value);
+
+    return { status, value };
+  }, [palette, paletteType]);
+}
+
+export default usePaletteCode;
